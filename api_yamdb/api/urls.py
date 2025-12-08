251a5f89a5170f07b216da6aeab962fd542b3bbd
Yausei Ilya <iliaevsei32@gmail.com>
2025-12-08 17:24:41 +0300

    /

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,18 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+#from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 
 router_v1 = routers.DefaultRouter()
-router_v1.register('posts', PostViewSet, basename='posts')
-router_v1.register('groups', GroupViewSet, basename='groups')
-router_v1.register(
-    r'posts/(?P<post_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments'
-)
-router_v1.register('follow', FollowViewSet, basename='follow')
+#router_v1.register('posts', PostViewSet, basename='posts')
+#router_v1.register('groups', GroupViewSet, basename='groups')
+#router_v1.register(
+#    r'posts/(?P<post_id>\d+)/comments',
+#    CommentViewSet,
+#    basename='comments'
+#)
+#router_v1.register('follow', FollowViewSet, basename='follow')
 
 
 urlpatterns = [
